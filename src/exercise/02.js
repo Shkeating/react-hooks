df7a45a501cfdafd1627fc2cda7b6c3533de656b
Shauna Keating <shauna@truss.works>
2022-01-18 19:37:58 -0500

    adjusts ex 2 for extra credit

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -6,13 +6,14 @@ import * as React from 'react'
 function Greeting({initialName = ''}) {
   // 🐨 initialize the state to the value from localStorage
   // 💰 window.localStorage.getItem('name') ?? initialName
+
   const [name, setName] = React.useState(
-    window.localStorage.getItem('name') ?? initialName,
+    () => window.localStorage.getItem('name') ?? initialName,
   )
 
   React.useEffect(() => {
     window.localStorage.setItem('name', name)
-  })
+  }, [name])
 
   function handleChange(event) {
     setName(event.target.value)
